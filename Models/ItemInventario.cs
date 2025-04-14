4a2637af0005dfc7135234d5b1c5d29bb2c65129
marcos <marcosbentivegna@gmail.com>
2025-04-14 12:28:20 +0200

    Implementación atributos de ItemInventario

--- Models/ItemInventario.cs
+++ Models/ItemInventario.cs
@@ -2,5 +2,9 @@
 {
     public class ItemInventario
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public string Estado { get; set; }
     }
 }
